b9a63ad792304137cd47430e2fabfde47b0dff7f
jflute <dbflute@gmail.com>
2018-01-25 20:04:19 +0900

    thymeleaf3 trial now

---
+++ src/test/java/org/lastaflute/thymeleaf/trial/ThymeleafPlainTrialTest.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2015-2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.lastaflute.thymeleaf.trial;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
+import org.dbflute.utflute.core.PlainTestCase;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.TemplateSpec;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.context.IContext;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.FileTemplateResolver;
+
+/**
+ * @author jflute
+ */
+public class ThymeleafPlainTrialTest extends PlainTestCase {
+
+    private static final String BASE_DIR = "/target/test-classes/trial-templates";
+
+    public void test_trial1() throws IOException {
+        // ## Arrange ##
+        TemplateEngine engine = new TemplateEngine();
+        engine.addTemplateResolver(new FileTemplateResolver());
+
+        // ## Act ##
+        String canonicalPath = getProjectDir().getCanonicalPath();
+        String template = canonicalPath + BASE_DIR + "/thymeleaf-trial1.html";
+        Context context = new Context();
+        String processed = engine.process(template, context);
+
+        // ## Assert ##
+        log(ln() + processed);
+    }
+
+    public void test_trial2() throws Exception {
+        // ## Arrange ##
+        TemplateEngine engine = new TemplateEngine();
+        engine.addTemplateResolver(new FileTemplateResolver());
+
+        // ## Act ##
+        String canonicalPath = getProjectDir().getCanonicalPath();
+
+        Map<String, Object> variableMap = new HashMap<>();
+        HeaderBean headerBean = new HeaderBean();
+        headerBean.isLogin = true;
+        headerBean.memberName = "judy suto";
+        variableMap.put("title", "jiro-san, ramen daisuki");
+        variableMap.put("headerBean", headerBean);
+
+        IContext context = new Context(Locale.ENGLISH, variableMap);
+        String template = canonicalPath + BASE_DIR + "/thymeleaf-trial2.html";
+        String processed = engine.process(new TemplateSpec(template, TemplateMode.XML), context);
+
+        // ## Assert ##
+        log(ln() + processed);
+    }
+
+    public static class HeaderBean {
+
+        public boolean isLogin;
+        public String memberName;
+    }
+}
