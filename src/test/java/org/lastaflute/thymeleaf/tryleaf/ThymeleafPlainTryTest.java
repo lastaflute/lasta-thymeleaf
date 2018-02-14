111db4f2e476b60b5ea688c2d1f38e15980548c2
jflute <dbflute@gmail.com>
2018-02-14 17:54:58 +0900

    rename package to tryleaf

--- src/test/java/org/lastaflute/thymeleaf/tryleaf/ThymeleafPlainTryTest.java
+++ src/test/java/org/lastaflute/thymeleaf/tryleaf/ThymeleafPlainTryTest.java
@@ -13,7 +13,7 @@
  * either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
-package org.lastaflute.thymeleaf.trial;
+package org.lastaflute.thymeleaf.tryleaf;
 
 import java.io.IOException;
 import java.util.HashMap;
